59221f495cdd54e993d98580611236c01354fee8
codewithdary <info@darynazar.com>
2023-03-29 16:29:59 +0200

    [EP 62] Pruning Models

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('model:prune')->dailyAt('2:30');
     }
 
     /**
