84dc643ab11e01f7bc254cd03a359d1292ac4658
codewithdary <info@darynazar.com>
2023-03-29 14:59:12 +0200

    [EP 21] Using a JSON file to read data

--- database/seeders/PostsTableSeeder.php
+++ database/seeders/PostsTableSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Post;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
 
 class PostsTableSeeder extends Seeder
 {
@@ -13,27 +14,18 @@ class PostsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $posts = collect([
-            [
-                'title' => 'Post One',
-                'slug' => 'post-one',
-                'excerpt' => 'Excerpt of Post One',
-                'description' => 'Description of Post One',
-                'is_published' => true,
-                'min_to_read' => 2
-            ],
-            [
-                'title' => 'Post Two',
-                'slug' => 'post-two',
-                'excerpt' => 'Excerpt of Post Two',
-                'description' => 'Description of Post Two',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ]
-        ]);
+        $json = File::get('database/json/posts.json');
+        $posts = collect(json_decode($json));
 
         $posts->each(function ($post) {
-            Post::create($post);
+            Post::insert([
+                "title" => $post->title,
+                "slug" => $post->slug,
+                "excerpt" => $post->excerpt,
+                "description" => $post->description,
+                "is_published" => $post->is_published,
+                "min_to_read" => $post->min_to_read
+            ]);
         });
     }
 }
