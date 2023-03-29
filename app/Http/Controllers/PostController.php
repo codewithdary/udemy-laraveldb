fe71cd0e59d28f201ed40473dd7dc167a5b0edd6
codewithdary <info@darynazar.com>
2023-03-29 17:03:35 +0200

    [EP 73] Has one Through

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
+use App\Models\PhoneNumber;
 use App\Models\Post;
 use App\Models\User;
 
@@ -9,10 +11,22 @@ class PostController extends Controller
 {
     public function index()
     {
-        // Lazy Loading
-        Post::all();
+        // Create a new Company
+        $company = Company::create([
+            'name' => 'Apple',
+            'user_id' => 5,
+        ]);
 
-        // Eager Loading
-        Post::with('tags')->get();
+        // Create a new Phone Number
+        PhoneNumber::create([
+            'number' => 48593953,
+            'company_id' => $company->id
+        ]);
+
+        // Retrieve phone number of the company where the use works
+        $user = User::find(5)->companyPhoneNumber()->get();
+
+        // Use Eager Loading
+        $user = User::with('companyPhoneNumber')->get();
     }
 }
---
+++ database/migrations/2023_03_29_150050_create_phone_numbers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('phone_numbers', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->foreignId('company_id')
+                ->constrained('companies')
+                ->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('phone_numbers');
+    }
+};
