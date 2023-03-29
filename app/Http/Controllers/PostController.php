e6741f5a387083b98696096182003fc71dd63ce1
codewithdary <info@darynazar.com>
2023-03-29 16:16:25 +0200

    [EP 54] Retrieving Single Models

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,9 +8,17 @@ class PostController extends Controller
 {
     public function index()
     {
-        Post::where('is_published', true)
-            ->where('min_to_read', '>', 5)
-            ->orderBy('title', 'desc')
-            ->get();
+        // Retrieve based on the primary key (RETURNS NULL)
+        Post::find(1000);
+
+        // Retrieve based on the primary key (RETURNS ERROR)
+        Post::findOrFail(1000);
+
+        // Retrieve based on a condition
+        Post::where('slug', 'ullam-rerum-rem-esse-voluptatem-non-necessitatibus-iste-hic')->first();
+        Post::firstWhere('slug', 'est-cum-odit-tempora-voluptates-quis-eligendi');
+
+        // Retrieve a specific row based on a custom attribute
+        Post::where('slug', 'est-cum-odit-tempora-voluptates-quis-eligendi')->firstOrFail();
     }
 }
