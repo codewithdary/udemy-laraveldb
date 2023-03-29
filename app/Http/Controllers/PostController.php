edf6af118fe503f82e04b21398ac43b8821ad8f2
codewithdary <info@darynazar.com>
2023-03-29 15:47:09 +0200

    [EP 41] Full Text Indexes

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,32 +9,15 @@ class PostController extends Controller
 {
     public function index()
     {
-        // selectRaw()
+        // whereFullText()
         DB::table('posts')
-            ->selectRaw('count(*) as post_count')
-            ->first();
-
-        // whereRaw()
-        DB::table('posts')
-            ->whereRaw('created_at > NOW() - INTERVAL 1 DAY')
+            ->whereFullText('description', 'quo')
             ->get();
 
-        // havingRaw()
+        // orWhereFullText()
         DB::table('posts')
-            ->select('user_id', DB::raw('SUM(min_to_read) as total_time'))
-            ->groupBy('user_id')
-            ->havingRaw('SUM(min_to_read) > 10')
-            ->get();
-
-        // orderByRaw()
-        DB::table('posts')
-            ->orderByRaw('created_at DESC')
-            ->get();
-
-        // groupByRaw()
-        DB::table('posts')
-            ->select('user_id', DB::raw('AVG(rating) as avg_rating'))
-            ->groupByRaw('user_id')
+            ->whereFullText('description', 'quo')
+            ->orWhereFullText('description', 'Doloribus')
             ->get();
     }
 }
