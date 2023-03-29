337e2f98243288965ac4d0d07e354cffc0971189
codewithdary <info@darynazar.com>
2023-03-29 15:18:10 +0200

    [EP 28] Insert & Get Id Through the Query Builder

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,61 +8,14 @@ class PostController extends Controller
 {
     public function index()
     {
-        // insert()
-        DB::table('posts')->insert([
-            [
-                'user_id' => 1,
-                'title' => 'Insert through the DB facade 2',
-                'slug' => 'insert-through-the-db-facade-2',
-                'excerpt' => 'excerpt',
-                'description' => 'description',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ],
-            [
-                'user_id' => 1,
-                'title' => 'Insert through the DB facade 3',
-                'slug' => 'insert-through-the-db-facade-3',
-                'excerpt' => 'excerpt',
-                'description' => 'description',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ]
+        DB::table('posts')->insertGetId([
+            'user_id' => 1,
+            'title' => 'Insert through the insertGetId',
+            'slug' => 'insert-through-the-insertgetid',
+            'excerpt' => 'excerpt',
+            'description' => 'description',
+            'is_published' => true,
+            'min_to_read' => 2,
         ]);
-
-        // insertOrIgnore()
-        DB::table('posts')->insertOrIgnore([
-            [
-                'user_id' => 1,
-                'title' => 'X',
-                'slug' => 'x',
-                'excerpt' => 'excerpt',
-                'description' => 'description',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ],
-            [
-                'user_id' => 1,
-                'title' => 'Insert through the DB facade 3',
-                'slug' => 'insert-through-the-db-facade-3',
-                'excerpt' => 'excerpt',
-                'description' => 'description',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ]
-        ]);
-
-        // upsert()
-        $posts = DB::table('posts')->upsert([
-            [
-                'user_id' => 1,
-                'title' => 'X',
-                'slug' => 'x',
-                'excerpt' => 'Updated excerpt',
-                'description' => 'Updated description',
-                'is_published' => true,
-                'min_to_read' => 2,
-            ],
-        ], ['title', 'slug']);
     }
 }
